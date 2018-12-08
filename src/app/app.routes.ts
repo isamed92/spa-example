4dfc60f8ef41c7b9963f3c2cbb01036f77a872c9
IsaiasPC <isamed92@gmail.com>
2018-12-08 00:30:04 -0300

    spa without list of heroes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroe/:id', component: HeroeComponent },
+    { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(ROUTES)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
+
+
---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: []
+})
+export class HeroeComponent  {
+  
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe(params => {
+      console.log(params);
+    });
+   }
+
+}
